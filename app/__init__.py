f85951390174149148d138d4bf3dd162bef3ce75
Jhonjs <jhon.sidney@gmail.com>
2024-12-05 19:53:16 -0300

    Update __init__.py

---
+++ app/__init__.py
@@ -0,0 +1,23 @@
+from flask import Flask
+from flask_bootstrap import Bootstrap
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from config import Config
+
+# Inicializa o objeto db
+db = SQLAlchemy()
+migrate = Migrate()
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config)  # Carrega a configuração do arquivo config.py
+
+    # Inicializa o db com o app
+    db.init_app(app)
+    migrate.init_app(app, db)
+
+    # Importa e registra as rotas principais do app
+    from . import routes
+    routes.init_app(app)
+
+    return app
